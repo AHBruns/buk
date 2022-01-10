9f63d9abc22351d5f48f0edce5dcbc84da1a1fff
Alex Bruns <alex.h.bruns@gmail.com>
2022-01-10 15:12:22 -0600

    client setup
    Signed-off-by: Alex Bruns <alex.h.bruns@gmail.com>

---
+++ client/src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="text-6xl text-red-400 tracking-wider leading-tight font-bold absolute inset-0 flex items-center justify-center">
+      Buk is coming soon!
+    </div>
+  );
+}
